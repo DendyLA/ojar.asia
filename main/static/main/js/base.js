e4501a9ea2714c3d2cde54fc1f04a001cad4e420
Dawa <atayewdawlet@gmail.com>
2025-07-18 12:48:06 +0500

    adding a hamburger to the mobile version

--- main/static/main/js/base.js
+++ main/static/main/js/base.js
@@ -1,49 +1,64 @@
 // Обработчик клика ТОЛЬКО для ссылок с классом activeNav
 document.querySelectorAll('.activeNav').forEach(link => {
-  link.addEventListener('click', function(e) {
-    e.preventDefault();
-    const navItem = this.closest('.nav__item');
-    const subNav = navItem.querySelector('.sub-nav');
+	link.addEventListener('click', function(e) {
+		e.preventDefault();
+		const navItem = this.closest('.nav__item');
+		const subNav = navItem.querySelector('.sub-nav');
 
-    // Закрываем все другие подменю
-    document.querySelectorAll('.sub-nav.active').forEach(menu => {
-      if (menu !== subNav) menu.classList.remove('active');
-    });
+		// Закрываем все другие подменю
+		document.querySelectorAll('.sub-nav.active').forEach(menu => {
+		if (menu !== subNav) menu.classList.remove('active');
+		});
 
-    // Открываем/закрываем текущее подменю
-    subNav.classList.toggle('active');
-  });
+		// Открываем/закрываем текущее подменю
+		subNav.classList.toggle('active');
+	});
 });
 
 // Закрытие при клике вне меню
 document.addEventListener('click', function(e) {
-  if (!e.target.closest('.nav__item')) {
-    document.querySelectorAll('.sub-nav.active').forEach(menu => {
-      menu.classList.remove('active');
-    });
-  }
+	if (!e.target.closest('.nav__item')) {
+		document.querySelectorAll('.sub-nav.active').forEach(menu => {
+		menu.classList.remove('active');
+		});
+	}
+	});
+
+
+	//СКРОЛЛ
+
+	// Получаем элементы
+	const header = document.querySelector('header');
+	const headerHeight = header.offsetHeight;
+	document.documentElement.style.setProperty('--header-height', `${headerHeight}px`);
+
+	// Создаем placeholder
+	const placeholder = document.createElement('div');
+	placeholder.className = 'header-placeholder';
+	header.insertAdjacentElement('afterend', placeholder);
+
+	// Обработчик скролла
+	window.addEventListener('scroll', function() {
+	const scrollPosition = window.scrollY || window.pageYOffset;
+
+	if (scrollPosition > headerHeight) {
+		header.classList.add('fixed');
+	} else {
+		header.classList.remove('fixed');
+	}
 });
 
 
-//СКРОЛЛ
+function toogleBurger( burger, header ){
+	burger.addEventListener('click', () => {
+		burger.classList.toggle('is-active');
+		header.classList.toggle('header__active');
+	})
+	
+}
 
-// Получаем элементы
-const header = document.querySelector('header');
-const headerHeight = header.offsetHeight;
-document.documentElement.style.setProperty('--header-height', `${headerHeight}px`);
+const burger = document.querySelector('.hamburger');
+const headerRight = document.querySelector('.header__right')
 
-// Создаем placeholder
-const placeholder = document.createElement('div');
-placeholder.className = 'header-placeholder';
-header.insertAdjacentElement('afterend', placeholder);
+toogleBurger(burger, headerRight)
 
-// Обработчик скролла
-window.addEventListener('scroll', function() {
-  const scrollPosition = window.scrollY || window.pageYOffset;
-  
-  if (scrollPosition > headerHeight) {
-    header.classList.add('fixed');
-  } else {
-    header.classList.remove('fixed');
-  }
-});
